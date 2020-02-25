ffb0ccbe8d17401f4b4002c722f54b830bd2792d
molezz <molezzz@gmail.com>
2020-02-25 10:02:03 +0800

    运行时自动同步数据库 尝试修正投票页面错误

--- docker/rails/production-entrypoint.sh
+++ docker/rails/production-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -f /var/leave/tmp/pids/server.pid \
+&& RAILS_ENV=production /var/leave/bin/rails db:migrate \
 && ls /var/leave/public/packs \
 && RAILS_ENV=production /var/leave/bin/rails webpacker:compile \
 && /var/leave/bin/rails s -e production -p 3000
