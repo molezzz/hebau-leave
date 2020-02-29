d98a1d18ccf9b2fd27dbc7bfc8c0c49bbcd719bd
molezz <molezzz@gmail.com>
2020-02-29 23:23:13 +0800

    去掉手机号的空格

--- app/controllers/assess_controller.rb
+++ app/controllers/assess_controller.rb
@@ -135,13 +135,13 @@ class AssessController < ApplicationController
       category = params[:category].to_i
       render(json: { error: true, message: '请选择正确的部门类别' }) && return unless [0,1,2,3].include? category
       codes = params[:mobiles].split(',').map do |m|
-        VoteMember.new(mobile: m, code: SecureRandom.hex(8), kind: kind, category: category)
+        VoteMember.new(mobile: m.gsub(/\s+/,''), code: SecureRandom.hex(8), kind: kind, category: category)
       end
       result = VoteMember.import codes
       render json: { failed: result.failed_instances.map{|ins| ins.mobile }}
     else
       codes = params[:mobiles].split(',').map do |m|
-        VoteMember.new(mobile: m, code: SecureRandom.hex(8), kind: kind, department_id: params[:department])
+        VoteMember.new(mobile: m.gsub(/\s+/,''), code: SecureRandom.hex(8), kind: kind, department_id: params[:department])
       end
       result = VoteMember.import codes
       render json: { failed: result.failed_instances.map{|ins| ins.mobile }}
